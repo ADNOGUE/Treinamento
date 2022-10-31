1609bca1869b40526639deed106675bed7781d83
adnogue <dri.nogueira.nagata@gmail.com>
2022-10-31 12:18:07 -0300

    correcao

--- Exercicios/br/com/letscode/funcionarios/FuncionarioMain.java
+++ Exercicios/br/com/letscode/funcionarios/FuncionarioMain.java
@@ -11,8 +11,49 @@ public class FuncionarioMain {
         coordenador.cpf  = "297.218.478.52";
         coordenador.salarioBase = 1000;
 
-        coordenador.imprimirToString();
+        Funcionario gerente = new Funcionario();
+        gerente.nome = "Rodrigo";
+        gerente.cpf  = "299.288.498.55";
+        gerente.salarioBase = 20000;
 
+        coordenador.imprimirToString();
+        gerente.imprimirToString();
+
+        if ( coordenador.equals(gerente) ) { //comparacao de objetos
+            System.out.println("sao iguais");
+        } else {
+            System.out.println("sao diferentes");
+        }
+
+        if ( coordenador == gerente ) { // CUIDADO!! comparacao de referencia de memoria
+            System.out.println("sao iguais");
+        } else {
+            System.out.println("sao diferentes");
+        }
+
+        if ( coordenador.toString().equals(gerente.toString()) ) { //comparacao do toString
+            System.out.println("sao iguais");
+        } else {
+            System.out.println("sao diferentes");
+        }
+
+        if ( coordenador.hashCode() == gerente.hashCode() ) { // comparacao de hashCode
+            System.out.println("hash sao iguais");
+        } else {
+            System.out.println("sao diferentes");
+        }
+
+        if ( gerente instanceof Funcionario) {
+            System.out.println("gerente é Funcionario");
+        }
+
+        if ( coordenador instanceof Funcionario ) {
+            System.out.println("coordenador é Funcionario");
+        }
+
+        if ( gerente instanceof Object ) {
+            System.out.println("gerente é um Object");
+        }
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("digite a quantidade de dias trabalhados: ");
